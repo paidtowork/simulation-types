07dc0869ae49f2ea6a4b588587fc81fe24e3adb1
Arun Gandikota <arun553@gmail.com>
2024-06-12 16:02:48 -0400

    Added Result type

--- dist/simulation/response.d.ts
+++ dist/simulation/response.d.ts
@@ -1,4 +1,16 @@
 import * as protos from '@google-cloud/optimization/build/protos/protos';
+import { SIMULATION_STATUS_ENUM } from '../status';
+type ISO8601DateString = string;
+
+export type Result = {
+    simulationId: string;
+    solutionUrl?: string;
+    status: SIMULATION_STATUS_ENUM;
+    time: ISO8601DateString;
+    created_at: ISO8601DateString;
+    updated_at: ISO8601DateString;
+    userId: string;
+};
 
 export type SolutionResponse = {
     name: string;
---
+++ dist/status.d.ts
@@ -0,0 +1,5 @@
+export const SIMULATION_STATUS_ENUM = {
+    'pending': 0,
+    'success': 1,
+    'failure': 2,
+}
